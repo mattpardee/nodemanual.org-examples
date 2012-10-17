0979a98f80e7c2d5cfe2cdd17358aeb438b0a56f
Garen Torikian <gjtorikian@gmail.com>
2012-10-17 11:05:26 -0700

    Update for 0.8.12

--- nodejs_ref_guide/process/process.hrtime.js
+++ nodejs_ref_guide/process/process.hrtime.js
@@ -1,11 +1,12 @@
 // Exmaple: Passing in previous results to make benchmarks
-var t = process.hrtime();
+var time = process.hrtime();
 // [ 1800216, 927643717 ]
 
 setTimeout(function () {
-  t = process.hrtime(t);
+  var diff = process.hrtime(time);
   // [ 1, 6962306 ]
 
-  console.log('benchmark took %d seconds and %d nanoseconds', t[0], t[1]);
+  console.log('benchmark took %d seconds and %d nanoseconds',
+              diff[0], diff[1]);
   // benchmark took 1 seconds and 6962306 nanoseconds
 }, 1000);
